eed15e7f2d14e38b866cc2ab202ed6ff9f216e67
mao13132 <mao13132@gmail.com>
2025-11-15 10:21:23 +0300

    feat: clear bet

---
+++ src/business/bet_menu/clear_bet/clear_bet_call_.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------
+# Program by @developer_telegrams
+#
+#
+# Version   Date        Info
+# 1.0       2023    Initial Version
+#
+# ---------------------------------------------
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from settings import LOGO
+from src.telegram.keyboard.keyboards import Admin_keyb
+from src.telegram.sendler.sendler import Sendler_msg
+
+from src.telegram.bot_core import BotDB
+
+
+async def clear_bet_call(call: types.CallbackQuery, state: FSMContext):
+    await state.finish()
+
+    res_clear = await BotDB.user_messages.delete_by_filter({})
+
+    keyboard = Admin_keyb().bet_keyboard()
+
+    _msg = f'✅ Успешно очистил загруженный прогноз'
+
+    await Sendler_msg().sendler_photo_call(call, LOGO, _msg, keyboard)
+
+    return True
+
